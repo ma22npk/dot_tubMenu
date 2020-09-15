d8c342f49d49b6ced3f5704c7957bcfe73345916
Kota Matsumoto <mac@mba.local>
2020-09-15 13:30:24 +0900

    タブの内容を切り替えよう

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-'use strict';
-{
-const menuItems = document.querySelectorAll('.menu li a');
-menuItems.forEach(item => {
-item.addEventListener('click', e =>{
-e.preventDefault();
-item.classList.add('active');
-
-})
-
-});
-
+'use strict'; {
+  const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+      clickedItem.classList.add('active');
+      contents.forEach(content => {
+        content.classList.remove('active');
+      })
+    })
+  });
 }
